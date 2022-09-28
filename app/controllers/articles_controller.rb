c66019f16525b480b0071947ddd1e187765b9ac0
elyamurza <elya.murza@huntresslabs.com>
2022-09-28 16:26:49 -0700

    some changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,5 +9,25 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
 
+  def edit
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else 
+      render 'new'
+    end
+  end
+
+  def update
+
+  end
 end
